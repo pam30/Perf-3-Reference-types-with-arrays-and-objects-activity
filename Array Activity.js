a141817f12e3b411100a22f5e94fce3022a060fe
pam30 <202375371+pam30@users.noreply.github.com>
2025-07-11 13:28:54 +0800

    Create Array Activity.js

---
+++ Array Activity.js
@@ -0,0 +1,19 @@
+// Activity 1: Working with Arrays
+
+// Create an array called favSingers and store your three favorite singers
+const favSingers = ["Phil Wickham ", "Chris Tomlin", "Darlene Zschech"];
+
+
+console.log("First favorite singer:", favSingers[0]);
+
+// Create an array called favNumbers and store four of your favorite numbers
+const favNumbers = [7, 11, 42, 99];
+console.log("Favorite numbers:", favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+// Access and log each item in the mixedArr using array index notation
+console.log("mixedArr[0]:", mixedArr[0]);
+console.log("mixedArr[1]:", mixedArr[1]);
+console.log("mixedArr[2]:", mixedArr[2]);
+console.log("mixedArr[3]:", mixedArr[3]);
